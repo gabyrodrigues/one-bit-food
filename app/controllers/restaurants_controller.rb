47592847ed41b79f32dc0f1633861674785d9dfc
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2021-05-16 02:53:58 -0300

    filtrando restaurantes por nome, descrição, cidade e categoria

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,6 +3,9 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurants = Restaurant.all
+    filter_by_query if params[:query]
+    filter_by_city if params[:city]
+    filter_by_category if params[:category]
   end
 
   def show
@@ -11,5 +14,19 @@ class RestaurantsController < ApplicationController
   private
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def filter_by_query
+    @restaurants = @restaurants.ransack(name_or_description_cont: params[:query]).result #busca restaurante por nome ou descrição
+  end
+
+  def filter_by_city
+    @restaurants = @restaurants.where(city: params[:city])
+  end
+
+  def filter_by_category
+    @restaurants = @restaurants.select do |restaurant| ##seleciona apenas os restaurantes com a categoria do parametro
+      restaurant.category.title == params[:category]
+    end
   end
 end
